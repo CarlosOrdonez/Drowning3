1bd2b6caead97e24bd0cbe45548edd48d429f7a9
CarlosOrdonez <54687569+CarlosOrdonez@users.noreply.github.com>
2020-01-24 15:38:22 -0800

    Add files via upload

---
+++ drowning2.rb
@@ -0,0 +1,97 @@
+use_bpm 129
+sample "C:/Users/carlos_ordonez/Downloads/challenge_d/travis_sounds/khaled1.wav"
+sample_duration"C:/Users/carlos_ordonez/Downloads/challenge_d/travis_sounds/khaled1.wav"
+sleep 8
+live_loop:reel do
+  play :e4
+  play :g4
+  play:c3
+  sleep 0.5
+  play :c4
+  sleep 0.5
+  play :e4
+  play :g4
+  sleep 0.5
+  play :c4
+  sleep 0.5
+  play :e4
+  play :g4
+  sleep 0.5
+  play :c4
+  sleep 0.5
+  play :e4
+  play :g4
+  sleep 0.5
+  play :c4
+  sleep 0.5
+  play :e4
+  play :g4
+  play :c3
+  sleep 0.5
+  play :c4
+  sleep 0.5
+  play :e4
+  play :g4
+  sleep 0.5
+  play :c4
+  sleep 0.5
+  play :e4
+  play :a4
+  play :a3
+  sleep 0.5
+  play :c4
+  sleep 0.5
+  play :e4
+  play :g4
+  play :g3
+  sleep 0.5
+  play :c4
+  sleep 0.5
+  play :g4
+  play :d4
+  sleep 0.5
+  play :c4
+  sleep 0.5
+  play :g4
+  play :d4
+  sleep 0.5
+  play :c4
+  sleep 0.5
+  play :g4
+  play :d4
+  sleep 0.5
+  play :c4
+  sleep 0.5
+  play :g4
+  play :d4
+  play :b2
+  sleep 0.5
+  play :c4
+  sleep 0.5
+  play :g4
+  play :d4
+  sleep 0.5
+  play :c4
+  sleep 0.5
+  play :a4
+  play :d4
+  play :a3
+  sleep 0.5
+  play :c4
+  sleep 0.5
+  play :g4
+  play :d4
+  play :g3
+  sleep 0.5
+  play :c4
+  sleep 0.5
+end
+
+
+
+
+
+
+
+
+
